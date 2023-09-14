ef69a0eb4ea41a05c95ef9ca17a8f6d94d48a44d
Oleksii <zelenkooleksii75@gmail.com>
2023-09-14 19:18:16 +0300

    added planner event

--- pages/plannerCalendar.vue
+++ pages/plannerCalendar.vue
@@ -11,7 +11,7 @@
             color="grey darken-2"
             @click="setToday"
           >
-            Today
+            Сьогодні
           </v-btn>
           <v-btn
             fab
@@ -84,7 +84,6 @@
           @click:event="showEvent"
           @click:more="viewDay"
           @click:date="viewDay"
-          @change="updateRange"
         />
         <v-menu
           v-model="selectedOpen"
@@ -104,7 +103,7 @@
               <v-btn icon>
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name" />
+              <v-toolbar-title>{{ selectedEvent.name }}</v-toolbar-title>
               <v-spacer />
               <v-btn icon>
                 <v-icon>mdi-heart</v-icon>
@@ -114,7 +113,15 @@
               </v-btn>
             </v-toolbar>
             <v-card-text>
-              <span v-html="selectedEvent.details" />
+              <span>{{ selectedEvent.details }}</span>
+              <div style="color: grey">
+                <span>Номер пісні - </span>
+                <span>{{ selectedEvent.order || 1 }}</span>
+              </div>
+              <div style="color: grey">
+                <span>Назва категорії - </span>
+                <span>{{ selectedEvent.category }}</span>
+              </div>
             </v-card-text>
             <v-card-actions>
               <v-btn
@@ -122,7 +129,7 @@
                 color="secondary"
                 @click="selectedOpen = false"
               >
-                Cancel
+                Закрити
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -138,17 +145,26 @@ export default {
     focus: '',
     type: 'month',
     typeToLabel: {
-      month: 'Month',
-      week: 'Week',
-      day: 'Day',
+      month: 'Місяць',
+      week: 'Неділя',
+      day: 'День',
       '4day': '4 Days'
     },
     selectedEvent: {},
     selectedElement: null,
     selectedOpen: false,
-    events: [],
-    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party']
+    events: [
+      {
+        name: 'Слава Богу',
+        category: 'Поклоніння',
+        start: '2023-09-03 10:10:00',
+        end: '2023-09-03 11:00:00',
+        color: 'orange',
+        timed: false,
+        order: 2
+      }
+    ],
+    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1']
   }),
   mounted () {
     this.$refs.calendar.checkChange()
@@ -186,35 +202,6 @@ export default {
       }
 
       nativeEvent.stopPropagation()
-    },
-    updateRange ({ start, end }) {
-      const events = []
-
-      const min = new Date(`${start.date}T00:00:00`)
-      const max = new Date(`${end.date}T23:59:59`)
-      const days = (max.getTime() - min.getTime()) / 86400000
-      const eventCount = this.rnd(days, days + 20)
-
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-        const second = new Date(first.getTime() + secondTimestamp)
-
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay
-        })
-      }
-
-      this.events = events
-    },
-    rnd (a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a
     }
   }
 }
